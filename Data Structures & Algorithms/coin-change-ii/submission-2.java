6a16679284cdceecd114ee931f9261a94d22c115
Sumita Kumari Jha <121679220+Sumita2611@users.noreply.github.com>
2026-09-25 06:11:06 +0530

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        for(int i = 0;i <= amount;i++){
+            if(i % coins[0] == 0){
+                dp[0][i] = 1;
+            }
+        }
+        for(int ind = 1;ind < n;ind++){
+            for(int target = 0;target <= amount;target++){
+                int notTake = dp[ind-1][target];
+                int take = 0;
+                if(coins[ind] <= target){
+                    take = dp[ind][target-coins[ind]];
+                }
+                dp[ind][target] = take + notTake;
+            }
+        }
+        return dp[n-1][amount];
+    }
+}
